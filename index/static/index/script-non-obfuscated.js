7b443b59aa7d9c3c178bc96598dc493cffbecf80
mukulbindal <mukulbindal170299@gmail.com>
2020-05-01 13:35:07 +0530

    Optimization and obfuscation

---
+++ index/static/index/script-non-obfuscated.js
@@ -0,0 +1,27 @@
+function getimage(event)
+		{
+			if(!(event==null || event.keyCode==32 || event.keyCode==13 || event.keyCode==190))return;
+			$text = document.getElementById("text").value;
+			$fontwidth = document.getElementById("font-width").value;
+			$fontheight = document.getElementById("font-height").value;
+			$tableft = document.getElementById("tab-left").value;
+			$tabright = document.getElementById("tab-right").value;
+			$fontsize = document.getElementById("font-size").value;
+			$fontcolor = document.getElementById("font-color").value;
+			$.get('/write/',
+				{
+				string:$text,
+				fontwidth: $fontwidth,
+				fontheight: $fontheight,
+				tableft: $tableft,
+				tabright:$tabright,
+				fontsize:$fontsize,
+				fontcolor:$fontcolor
+				},
+				function(data)
+				{
+var img = document.getElementById("op");
+img.src = 'data:image/jpeg;base64,' + data;
+				}
+			);
+		}
